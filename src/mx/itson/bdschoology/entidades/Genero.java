66a345f752fcceadd76a66bd912f2c7bfc3b23ef
enriquefelix876 <enriquefelix876@gmail.com>
2017-11-30 18:08:38 -0700

    Actualización de Entidad Usuario

--- src/mx/itson/bdschoology/entidades/Genero.java
+++ src/mx/itson/bdschoology/entidades/Genero.java
@@ -2,20 +2,14 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */ 
+ */
 package mx.itson.bdschoology.entidades;
 
 /**
  *
  * @author enriq_000
  */
-public class Usuario {
+public enum Genero {
     
-    private int id;
-    private String nombre;
-    private String correo;
-    private String pass;
-    private Genero genero;
-    private TipoCuenta tipoCuenta;
-    
+    HOMBRE,MUJER
 }
--- src/mx/itson/bdschoology/entidades/TipoCuenta.java
+++ src/mx/itson/bdschoology/entidades/TipoCuenta.java
@@ -2,20 +2,14 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */ 
+ */
 package mx.itson.bdschoology.entidades;
 
 /**
  *
  * @author enriq_000
  */
-public class Usuario {
+public enum TipoCuenta {
     
-    private int id;
-    private String nombre;
-    private String correo;
-    private String pass;
-    private Genero genero;
-    private TipoCuenta tipoCuenta;
-    
+    ALUMNO, PROFESOR
 }
